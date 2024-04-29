9d97ee5bcf32d18cbf5d0dd60195068e60b5999e
Nikolai Shangin <shanginn@gmail.com>
2024-04-29 10:53:51 +0700

    Refactored backup script to use xz compression instead of 7z and updated the file extension accordingly. Added a command to the deployment.yaml file to keep the container running indefinitely. Removed the PGPASSWORD value from the helm values.yaml file as it will be fetched from the database secret.

--- backup.sh
+++ backup.sh
@@ -3,9 +3,9 @@ set -ex
 
 echo "Backup start at $(date)"
 
-pg_dumpall -h "$PGHOST" -p "$PGPORT" -U "$PGUSER" | 7z a -si db.7z
-echo "Backup created with size $(du -h db.7z | awk '{print $1}')"
+pg_dumpall -h "$PGHOST" -p "$PGPORT" -U "$PGUSER" | xz -9e > db.xz
+echo "Backup created with size $(du -h db.xz | awk '{print $1}')"
 
-wrangler r2 object put "pgsql-backup/db_$(date +%Y-%m-%d_%H_%M).7z" --file=db.7z
+wrangler r2 object put "pgsql-backup/db_$(date +%Y-%m-%d_%H_%M).xz" --file=db.xz
 
 echo "Backup complete at $(date)"
