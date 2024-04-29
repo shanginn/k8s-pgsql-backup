86fe82102439407e8ac1043846b176aa3bdc2a76
Nikolai Shangin <shanginn@gmail.com>
2024-04-29 14:23:47 +0700

    Refactor deploy.yaml, Dockerfile, backup.sh, cronjob.yaml, serviceaccount.yaml, and values.yaml to improve deployment process and add necessary environment variables for Cloudflare integration.

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,18 @@
-FROM oven/bun:latest
+FROM debian:stable-slim
 
-RUN bun install -g wrangler
 RUN apt-get update && \
     apt-get install -y postgresql-common gnupg && \
     /usr/share/postgresql-common/pgdg/apt.postgresql.org.sh -y && \
     apt-get install postgresql-15 -y && \
     apt-get clean
 
-RUN ln -s /usr/local/bin/bun /bin/node
+RUN curl -O https://downloads.rclone.org/rclone-current-linux-amd64.zip && \
+    unzip rclone-current-linux-amd64.zip && \
+    cp rclone-*-linux-amd64/rclone /usr/bin/ && \
+    rm -rf rclone-*-linux-amd64 rclone-current-linux-amd64.zip && \
+    chown root:root /usr/bin/rclone && \
+    chmod 755 /usr/bin/rclone
 
 COPY --link backup.sh /usr/local/bin/run-pgsql-backup
+
+ENTRYPOINT ["/usr/local/bin/run-pgsql-backup"]
--- backup.sh
+++ backup.sh
@@ -1,11 +1,28 @@
 #!/bin/bash
 set -ex
 
+export RCLONE_CONFIG_CLOUDFLARE_TYPE=s3
+export RCLONE_CONFIG_CLOUDFLARE_PROVIDER=Cloudflare
+export RCLONE_CONFIG_CLOUDFLARE_REGION=auto
+export RCLONE_CONFIG_CLOUDFLARE_ACL=private
+export RCLONE_CONFIG_CLOUDFLARE_ACCESS_KEY_ID=$CLOUDFLARE_ACCESS_KEY_ID
+export RCLONE_CONFIG_CLOUDFLARE_SECRET_ACCESS_KEY=$CLOUDFLARE_SECRET_ACCESS_KEY
+export RCLONE_CONFIG_CLOUDFLARE_ENDPOINT="https://${CLOUDFLARE_ACCOUNT_ID}.r2.cloudflarestorage.com/pgsql-backup"
+
+DB_NAME=$1
+FILENAME="$(date +%Y-%m-%d_%H_%M).xz"
+
+if [ -z "$DB_NAME" ]; then
+    echo "No database name provided. Exiting."
+    exit 1
+fi
+
 echo "Backup start at $(date)"
 
-pg_dumpall -h "$PGHOST" -p "$PGPORT" -U "$PGUSER" | xz -9e > db.xz
-echo "Backup created with size $(du -h db.xz | awk '{print $1}')"
+pg_dump -h "$PGHOST" -p "$PGPORT" -U "$PGUSER" -d "$DB_NAME" | xz -9e > "$FILENAME"
+echo "Backup created with size $(du -h "$FILENAME" | awk '{print $1}')"
 
-wrangler r2 object put "pgsql-backup/db_$(date +%Y-%m-%d_%H_%M).xz" --file=db.xz
+# name `cloudflare` comes from env variables names RCLONE_CONFIG_CLOUDFLARE_*
+rclone copy "$FILENAME" cloudflare:"${DB_NAME}/"
 
 echo "Backup complete at $(date)"
